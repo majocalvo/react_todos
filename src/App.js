7c86dd5e2b0fbbb7c6d0949f6debeb5d26f0a513
Maria Jose <mjcalvoalfaro@gmail.com>
2022-10-06 02:54:15 -0400

    desafio 3

--- src/App.js
+++ src/App.js
@@ -1,44 +1,78 @@
 import React, { useState } from "react";
+import "./App.css";
+import Title from "./componente/Title";
 
 function App() {
+  const [gastos, setGastos] = useState([]);
+
   return (
     <>
-      <Contador/>
-      <GastosMes/>
-      <AgregarGasto/>
+      <div className="App">
+        <header className="App-header">
+          <Title />
+          <Contador gastos_in={gastos} />
+          <GastosMes gastos_in={gastos} />
+          <AgregarGasto setGastos_in={setGastos} />
+          <Transacciones gastos_in={gastos} />
+        </header>
+      </div>
     </>
   );
 }
 
-function Contador (){
-  return <div> Gasto mensual: </div>;
-}
+function Contador({ gastos_in }) {
+  let total = 0;
 
-function GastosMes (){
-  const [gastos, setGastos] = useState([1500, 300, 450])
-
-return (
-  <ul>
-    {gastos.map ((gasto) =>( <li key = {gasto}>{gasto}</li>
-    ))}
-  </ul>
-);
-}
-
-function AgregarGasto () {
-  function handleSubmit (event) {
-    event.preventDefault ();
-    const gasto = event.target.elements.gasto.value;
-    console.log (gasto)
+  for (let i = 0; i < gastos_in.length; i++) {
+    total += gastos_in[i];
   }
 
-return (
-  <form onSubmit = {handleSubmit}>
-    <input autocomplete="off" type = "text" id = "gasto"/>
-    <button type="Submit"> Agregar gasto </button>
-  </form>
-)
+  return (
+    <div>
+      <div className="gasto_mensual">Gasto mensual: $ {total} </div>
+    </div>
+  );
 }
 
+function GastosMes({ gastos_in }) {
+  return (
+    <ul>
+      {gastos_in.map((gasto) => (
+        // <li key={gasto}>{gasto}</li>
+        <li className="lista">$ {gasto}</li>
+      ))}
+    </ul>
+  );
+}
+
+function AgregarGasto({ setGastos_in }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const gasto = event.target.elements.gasto.value;
+    console.log(gasto);
+    if (Number.isInteger(Number.parseInt(gasto))) {
+      setGastos_in((prevGastos) => [...prevGastos, Number.parseInt(gasto)]);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input className="input" autocomplete="off" type="text" id="gasto" />
+      <button className="boton" type="Submit">
+        {" "}
+        Agregar gasto{" "}
+      </button>
+    </form>
+  );
+}
+
+function Transacciones({ gastos_in }) {
+  return (
+    <div className="transacciones">
+      {" "}
+      Cantidad de transacciones: {gastos_in.length}
+    </div>
+  );
+}
 
 export default App;
---
+++ src/componente/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Title.css";
+
+function Title() {
+  return (
+    <div>
+      <h1 className="titulo">Desafio 3 : React_toDo</h1>
+    </div>
+  );
+}
+
+export default Title;
